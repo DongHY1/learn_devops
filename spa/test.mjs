60cf6537a8943397bab5fce43cc8d1030598e33a
donghy1 <915110101@qq.com>
2022-07-10 22:56:24 +0800

    删除测试文件,将URL放入env文件中

--- spa/test.mjs
+++
@@ -1,8 +0,0 @@
-import { resolve } from "path";
-import readdirp from "readdirp";
-const file = resolve("./dist", 'index.html');
-console.log(file)
-console.log(readdirp)
-for (const entry of readdirp("./dist", { depth: 0, type: "files" })) {
-    console.log(1)
-}
--- spa/vite.config.js
+++ spa/vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import dotenv from "dotenv";
+dotenv.config();
+console.log(process.env.BASE)
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'https://spa-1306502097.cos.ap-guangzhou.myqcloud.com/',
+  base:process.env.BASE,
   plugins: [vue()]
 })
