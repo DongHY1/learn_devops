45d12b8c6a120bc1ebe6a27d321011d94d95725d
donghy1 <915110101@qq.com>
2022-07-10 22:50:42 +0800

    成功部署资源到OSS上，配置base访问

--- spa/vite.config.js
+++ spa/vite.config.js
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'https://spa-1306502097.cos.ap-guangzhou.myqcloud.com/',
   plugins: [vue()]
 })
